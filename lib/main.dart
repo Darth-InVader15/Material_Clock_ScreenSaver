bf850b1f39d775dfb5057dae1b77b4498844c52c
Darth-InVader15 <piyush.singh1315@gmail.com>
2024-08-16 23:13:05 +0530

    add: gradient bg + better font

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,15 +20,18 @@ class TimeScreensaver extends StatefulWidget {
 
 class _TimeScreensaverState extends State<TimeScreensaver> {
   String _timeString = "";
+  double _gradientPosition = 0.0;
+  bool _increasing = true;
 
   @override
   void initState() {
     super.initState();
     _timeString = _formatTime(DateTime.now());
-    Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
+    Timer.periodic(Duration(minutes: 1), (Timer t) => _updateTime());
+    Timer.periodic(Duration(milliseconds: 50), (Timer t) => _updateGradient());
   }
 
-  void _getTime() {
+  void _updateTime() {
     final DateTime now = DateTime.now();
     final String formattedTime = _formatTime(now);
     setState(() {
@@ -35,23 +39,51 @@ class _TimeScreensaverState extends State<TimeScreensaver> {
     });
   }
 
+  void _updateGradient() {
+    setState(() {
+      if (_increasing) {
+        _gradientPosition += 0.01;
+        if (_gradientPosition >= 1.0) {
+          _increasing = false;
+        }
+      } else {
+        _gradientPosition -= 0.01;
+        if (_gradientPosition < -0.3) {
+          _increasing = true;
+        }
+      }
+    });
+  }
+
   String _formatTime(DateTime time) {
-    return "${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')}:${time.second.toString().padLeft(2, '0')}";
+    return "${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')}";
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          _timeString,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 80,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'Courier',
+      body: AnimatedContainer(
+        duration: Duration(milliseconds: 50),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.black, Colors.blue[900]!],
+            stops: [_gradientPosition, _gradientPosition + 0.5],
           ),
+        ),
+        child: Center(
+          child: Text(
+            _timeString,
+            style: GoogleFonts.honk(
+              color: Colors.white,
+              fontSize: 400,
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.center,
+
+          ),
+
         ),
       ),
     );
